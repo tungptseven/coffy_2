89d98fa9ba1e13cf3c63aac2c0dd432eb4c39897
tungptseven <tungpt.seven@gmail.com>
2017-04-18 11:31:20 +0700

    config for vps and mobile using

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ app.get('/detail/:id', (req, res) => {
  });*/
 
 
-app.post('/find/near', (req, res) => {
+app.post('/find/Near', (req, res) => {
     // let uLong = req.body['long'],
     //     uLat = req.body['lat'],
     //     uType = req.body['type'],
@@ -157,7 +157,7 @@ app.post('/find/near', (req, res) => {
 
                 //let myJson = result;
                 //console.log(myJson)
-                /*res.json(result)*/
+                //res.json(result)
             });
             // res.json(data);
             // success;
@@ -171,7 +171,7 @@ app.post('/find/near', (req, res) => {
     // res.json(results);
 });
 
-app.post('/find/dist', (req, res) => {
+app.post('/find/Dist', (req, res) => {
     let type = parseFloat(req.body['inType2']),
         district = parseFloat(req.body['inDist']);
     //let db = promise.db;
@@ -199,6 +199,83 @@ app.post('/find/dist', (req, res) => {
      res.json(results);*/
 });
 
+
+//REST for Mobile
+app.post('/find/near', (req, res) => {
+    // let uLong = req.body['long'],
+    //     uLat = req.body['lat'],
+    //     uType = req.body['type'],
+    //     uR = req.body['r'];
+    let uLong = parseFloat(req.body['inLong']),
+        uLat = parseFloat(req.body['inLat']),
+        uType = parseFloat(req.body['inType']),
+        uR = parseFloat(req.body['inR']);
+    console.log(uR, uType, uLong, uLat);
+
+    //let db = promise.db;
+    db.any(
+        //"SELECT id_location, name, address, octime, rate, lat, long, id_type, id_district FROM coffy.location WHERE ST_DWithin(geog, ST_Point(${uLong}, ${uLat})::geography, ${uR}) AND id_type = ${uType};",
+        "SELECT id_location, name, address, octime, rate, lat, long, id_type, id_district FROM coffy.location " +
+        "CROSS JOIN (SELECT ST_Point(${Long}, ${Lat})::geography AS ref_geog) As r WHERE ST_DWithin(geog, ref_geog, ${R}) " +
+        "AND id_type = ${Type} ORDER BY ST_Distance(geog, ref_geog);",
+        {
+            Long: uLong,
+            Lat: uLat,
+            R: uR,
+            Type: uType
+        })
+        .then(data => {
+            //console.log(data);
+            async.mapSeries(data, merge2, (err, result) => {
+                // let dt = {
+                //     'datas': result
+                // };
+                //res.render('index1.html', dt);
+
+                //let myJson = result;
+                //console.log(myJson)
+                res.json(result)
+            });
+            // res.json(data);
+            // success;
+        })
+        .catch(error => {
+            console.log(error);
+            // error;
+        });
+    // let results = search.findLoc(uLat, uLong, uType, uR);
+    // console.log(results);
+    // res.json(results);
+});
+
+app.post('/find/dist', (req, res) => {
+    let type = parseFloat(req.body['inType2']),
+        district = parseFloat(req.body['inDist']);
+    //let db = promise.db;
+    db.any("SELECT id_location, name, address, octime, rate, lat, long, id_type, id_district FROM coffy.location " +
+        "WHERE id_type = ${Type} AND id_district = ${District};",
+        {
+            District: district,
+            Type: type
+        })
+        .then(data => {
+            async.mapSeries(data, merge2, (err, result) => {
+                // let dt = {
+                //     'datas': result
+                // };
+                //res.render('index1.html', dt);
+                // success;
+                res.json(result)
+            })
+        })
+        .catch(error => {
+            console.log(error);
+            // error;
+        });
+    /*let results = search.findLocInDistrict(type, district);
+     console.log(results);
+     res.json(results);*/
+});
 
 function merge2(item, cb) {
     let id_location = item['id_location'];
@@ -232,3 +309,5 @@ function merge2(item, cb) {
             cb(err, null);
         });
 }
+
+
