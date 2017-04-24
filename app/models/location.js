5d6f8f3ee66673b6f2e751139c239154f5d19ef7
tungptseven <tungpt.seven@gmail.com>
2017-04-24 18:09:19 +0700

    refactor location.js

--- app/models/location.js
+++ app/models/location.js
@@ -6,147 +6,144 @@ const db = promise.db;
 const async = require('async');
 const Promise = require('bluebird');
 
-//Get list data of all Location function
-module.exports.getAll = () => {
-    return new Promise(function (resolve, reject) {
-        db.any("SELECT id_location, name, address, octime, rate, lat, long, id_type, id_district FROM coffy.location ORDER BY random() LIMIT 30")
-            .then(data => {
-                async.mapSeries(data, merge2, (err, result) => {
-                    dt = {
-                        'datas': result
-                    };
-                    resolve(dt)
-                });
-            })
-            .catch(error => {
-                reject(error);
-            });
-    })
+class Locations {
+    constructor(){
+    }
 
-}
-
-//Get data of single Location function
-module.exports.getDetail = (id) => {
-    return new Promise(function (resolve, reject) {
-        db.one("SELECT * FROM coffy.location WHERE id_location = ${id}", {id: id})
-            .then(data => {
-                let test = [data];
-                async.map(test, merge2, (err, result) => {
-                    let dt = {
-                        'data': result[0]
-                    };
-                    resolve(dt)
-                });
-            })
-            .catch(error => {
-                reject(error);
-            });
-    })
-}
-
-//Get list data of Neighbor location function
-module.exports.getNear = (uLat, uLong, uType, uR) => {
-    return new Promise(function (resolve, reject) {
-        db.any(
-            "SELECT id_location, name, address, octime, rate, lat, long, id_type, id_district FROM coffy.location " +
-            "CROSS JOIN (SELECT ST_Point(${Long}, ${Lat})::geography AS ref_geog) As r WHERE ST_DWithin(geog, ref_geog, ${R}) " +
-            "AND id_type = ${Type} ORDER BY ST_Distance(geog, ref_geog);",
-            {
-                Long: uLong,
-                Lat: uLat,
-                R: uR,
-                Type: uType
-            })
-            .then(data => {
-                async.mapSeries(data, merge2, (err, result) => {
-                    let dt = {
-                        'datas': result
-                    };
-                    resolve(dt)
-                });
-            })
-            .catch(error => {
-                reject(error);
-            });
-    })
-}
-
-
-//Get list data of Location in District function
-module.exports.getDist = (type, district) => {
-    return new Promise(function (resolve, reject) {
-        db.any("SELECT id_location, name, address, octime, rate, lat, long, id_type, id_district FROM coffy.location " +
-            "WHERE id_type = ${Type} AND id_district = ${District};",
-            {
-                Type: type,
-                District: district
-            })
-            .then(data => {
-                async.mapSeries(data, merge2, (err, result) => {
-                    let dt = {
-                        'datas': result
-                    };
-                    resolve(dt)
+    getAll() {
+        return new Promise(function (resolve, reject) {
+            let dt = '';
+            db.any("SELECT id_location, name, address, octime, rate, lat, long, id_type, id_district FROM coffy.location ORDER BY random() LIMIT 30")
+                .then(data => {
+                    async.mapSeries(data, merge2, (err, result) => {
+                        dt = {
+                            'datas': result
+                        };
+                        resolve(dt)
+                    });
                 })
-            })
-            .catch(error => {
-                reject(error);
-            });
-    })
-}
-
-//REST
-//Get Neighbor data
-module.exports.restLoc = (uLat, uLong, uType, uR) => {
-    return new Promise(function (resolve, reject) {
-        db.any(
-            "SELECT id_location, name, address, octime, rate, lat, long, id_type, id_district FROM coffy.location " +
-            "CROSS JOIN (SELECT ST_Point(${Long}, ${Lat})::geography AS ref_geog) As r WHERE ST_DWithin(geog, ref_geog, ${R}) " +
-            "AND id_type = ${Type} ORDER BY ST_Distance(geog, ref_geog);",
-            {
-                Long: uLong,
-                Lat: uLat,
-                R: uR,
-                Type: uType
-            })
-            .then(data => {
-                async.mapSeries(data, merge2, (err, result) => {
-                    let dt = {
-                        'datas': result
-                    };
-                    resolve(dt)
+                .catch(error => {
+                    reject(error);
                 });
-            })
-            .catch(error => {
-                reject(error);
-            });
-    })
-}
+        })
+    }
 
-//Get District data
-module.exports.restDist = (type, district) => {
-    return new Promise(function (resolve, reject) {
-        db.any("SELECT id_location, name, address, octime, rate, lat, long, id_type, id_district FROM coffy.location " +
-            "WHERE id_type = ${Type} AND id_district = ${District};",
-            {
-                Type: type,
-                District: district
-            })
-            .then(data => {
-                async.mapSeries(data, merge2, (err, result) => {
-                    let dt = {
-                        'datas': result
-                    };
-                    resolve(dt)
+    getDetail(id) {
+        return new Promise(function (resolve, reject) {
+            db.one("SELECT * FROM coffy.location WHERE id_location = ${id}", {id: id})
+                .then(data => {
+                    let test = [data];
+                    async.map(test, merge2, (err, result) => {
+                        let dt = {
+                            'data': result[0]
+                        };
+                        resolve(dt)
+                    });
                 })
-            })
-            .catch(error => {
-                reject(error);
-            });
-    })
+                .catch(error => {
+                    reject(error);
+                });
+        })
+    }
+
+    getNear(lat, long, type, r) {
+        return new Promise(function (resolve, reject) {
+            db.any(
+                "SELECT id_location, name, address, octime, rate, lat, long, id_type, id_district FROM coffy.location " +
+                "CROSS JOIN (SELECT ST_Point(${Long}, ${Lat})::geography AS ref_geog) As r WHERE ST_DWithin(geog, ref_geog, ${R}) " +
+                "AND id_type = ${Type} ORDER BY ST_Distance(geog, ref_geog);",
+                {
+                    Long: long,
+                    Lat: lat,
+                    R: r,
+                    Type: type
+                })
+                .then(data => {
+                    async.mapSeries(data, merge2, (err, result) => {
+                        let dt = {
+                            'datas': result
+                        };
+                        resolve(dt)
+                    });
+                })
+                .catch(error => {
+                    reject(error);
+                });
+        })
+    }
+
+    getDist(type, district) {
+        return new Promise(function (resolve, reject) {
+            db.any("SELECT id_location, name, address, octime, rate, lat, long, id_type, id_district FROM coffy.location " +
+                "WHERE id_type = ${Type} AND id_district = ${District};",
+                {
+                    Type: type,
+                    District: district
+                })
+                .then(data => {
+                    async.mapSeries(data, merge2, (err, result) => {
+                        let dt = {
+                            'datas': result
+                        };
+                        resolve(dt)
+                    })
+                })
+                .catch(error => {
+                    reject(error);
+                });
+        })
+    }
+
+    restLoc(lat, long, type, r){
+        return new Promise(function (resolve, reject) {
+            db.any(
+                "SELECT id_location, name, address, octime, rate, lat, long, id_type, id_district FROM coffy.location " +
+                "CROSS JOIN (SELECT ST_Point(${Long}, ${Lat})::geography AS ref_geog) As r WHERE ST_DWithin(geog, ref_geog, ${R}) " +
+                "AND id_type = ${Type} ORDER BY ST_Distance(geog, ref_geog);",
+                {
+                    Long: long,
+                    Lat: lat,
+                    R: r,
+                    Type: type
+                })
+                .then(data => {
+                    async.mapSeries(data, merge2, (err, result) => {
+                        let dt = {
+                            'datas': result
+                        };
+                        resolve(dt)
+                    });
+                })
+                .catch(error => {
+                    reject(error);
+                });
+        })
+    }
+
+    restDist(type, district){
+        return new Promise(function (resolve, reject) {
+            db.any("SELECT id_location, name, address, octime, rate, lat, long, id_type, id_district FROM coffy.location " +
+                "WHERE id_type = ${Type} AND id_district = ${District};",
+                {
+                    Type: type,
+                    District: district
+                })
+                .then(data => {
+                    async.mapSeries(data, merge2, (err, result) => {
+                        let dt = {
+                            'datas': result
+                        };
+                        resolve(dt)
+                    })
+                })
+                .catch(error => {
+                    reject(error);
+                });
+        })
+    }
 }
 
-function merge2(item, cb) {
+let merge2 = (item, cb) => {
     let id_location = item['id_location'];
     let id_type = item['id_type'];
     let id_district = item['id_district'];
@@ -178,3 +175,5 @@ function merge2(item, cb) {
             cb(err, null);
         });
 }
+
+module.exports = new Locations()
